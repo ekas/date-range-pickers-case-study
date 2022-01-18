2551aad461cc4271e90a650dcb5ac30204784940
Ekas Preet Singh <ekaspreet93.singh@gmail.com>
2022-01-18 16:55:57 +0100

    fix react jslancer picker

--- src/dates/google-flight/googleFlightDatePicker.js
+++ src/dates/google-flight/googleFlightDatePicker.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { RangeDatePicker } from "react-google-flight-datepicker";
 import "react-google-flight-datepicker/dist/main.css";
 
 import "./googleFlightDatePicker.css";
 
 const GoogleFlightDatePicker = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   return (
     <div className="google-flight-datepicker">
       <RangeDatePicker
-        startDate={new Date(2020, 0, 15)}
-        endDate={new Date(2020, 1, 1)}
+        startDate={startDate}
+        endDate={endDate}
+        onChange={(startDate, endDate) => {
+          setStartDate(startDate);
+          setEndDate(endDate);
+        }}
+        highlightToday={true}
       />
     </div>
   );
